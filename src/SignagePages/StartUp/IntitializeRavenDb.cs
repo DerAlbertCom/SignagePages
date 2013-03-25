7eb35ecf81d55d94b35b2ad6f9f9bed856b94b81
Albert Weinert <info@der-albert.com>
2013-03-25 20:10:48 +0100

    edited readme.md, enabled nuget packages restore

---
+++ src/SignagePages/StartUp/IntitializeRavenDb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Aperea.Infrastructure.Bootstrap;
+using Raven.Client;
+using Raven.Client.Embedded;
+using SignagePages.Settings;
+using StructureMap;
+
+namespace SignagePages.StartUp
+{
+    public class IntitializeRavenDb : BootstrapItem
+    {
+        readonly IContainer _container;
+
+        public IntitializeRavenDb(IContainer container)
+        {
+            _container = container;
+        }
+
+        static readonly Lazy<IDocumentStore> _lazyStore = new Lazy<IDocumentStore>(CreateDocumentStore);
+
+        static IDocumentStore CreateDocumentStore()
+        {
+            var settings = new RavenDbSettings();
+
+            var store = new EmbeddableDocumentStore
+                            {
+                                DataDirectory = settings.DataDirectory
+                            };
+            store.Initialize();
+            return store;
+        }
+
+        public override void Execute()
+        {
+            _container.Configure(c =>
+            {
+                c.For<IDocumentSession>().Singleton().Use(() => _lazyStore.Value.OpenSession());
+                c.For<IDocumentStore>().Singleton().Use(() => _lazyStore.Value);
+            });
+        }
+    }
+}
